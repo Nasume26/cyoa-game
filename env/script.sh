9c9a4211acd81d116ae20739a405e3d56f8b708f
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-09-09 09:32:34 -0500

    Added script to install dependancies and start gme

---
+++ env/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo ----Updating Packages------
+sudo apt-get update -y
+
+echo ----Installing Python-----
+sudo apt-get install software-properties-common -y
+
+echo -----Moving to App Folder-----
+cd ../vagrant/app
+pwd
+
+echo -----Starting game-----
+python3 game.py
